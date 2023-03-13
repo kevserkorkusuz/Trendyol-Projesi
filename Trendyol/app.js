f5aafe5f606e3f2b2a3e8509adcecf07ff048d1f
Kevser Korkusuz <kevserkorkusuz@gmail.com>
2023-03-13 16:38:07 +0300

    Trendyol Projesi
    HTML, CSS ve JS ile oluşturduğum Trendyol projesi

---
+++ Trendyol/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $('#carousel1').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:4
+            },
+            600:{
+                items:5
+            },
+            1000:{
+                items:10
+            }
+        }
+    })
+    $('#carousel2').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:5
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+
+   
+
+  });
